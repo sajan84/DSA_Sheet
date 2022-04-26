7b47ae8754c55701a42a01251fc7fa5d765bad19
Sajan Pande <87706744+sajan84@users.noreply.github.com>
2022-04-26 16:33:30 +0530

    Add files via upload

---
+++ move_all_negative_one_side.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+       int a[n];
+       int k=0;
+       for(int i=0;i<n;i++){
+           if(arr[i]>=0){
+               a[k++]=arr[i];
+           }
+       }
+       for(int i=0;i<n;i++){
+           if(arr[i]<0){
+               a[k++]=arr[i];
+           }
+       }
+       for(int i=0;i<n;i++){
+           arr[i]=a[i];
+       }
+    }
+};
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+  // } Driver Code Ends
